71d84142f1899c6d123896e46acb7e047b5d43db
Nortus22 <nortus22@gmail.com>
2021-11-09 16:24:55 -0800

    Added ThemeData

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:budgit/theme/themeData.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:budgit/widgets/dailyBudgetWidget.dart';
@@ -10,17 +11,52 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-          child: Column(
-        children: [
-          DailyBudget(),
-          SizedBox(
-            height: 30,
+    var size = MediaQuery.of(context).size;
+
+    return Stack(
+      children: [
+        Container(
+          color: AppColors.white,
+          height: 140,
+        ),
+        Positioned(
+          top: 140,
+          child: ClipRRect(
+            borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(60), right: Radius.circular(60)),
+            child: Container(
+              width: size.width,
+              height: size.height / 2,
+              color: AppColors.green,
+            ),
           ),
-          RemainingBudget()
-        ],
-      )),
+        ),
+        Positioned(
+          top: 140 * 2 + 50,
+          child: ClipRRect(
+            borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(60), right: Radius.circular(60)),
+            child: Container(
+              width: size.width,
+              height: size.height / 2 + 100,
+              color: AppColors.orange,
+            ),
+          ),
+        ),
+        Center(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            DailyBudget(),
+            SizedBox(
+              height: 30,
+            ),
+            RemainingBudget()
+          ],
+        )),
+      ],
     );
   }
 }
